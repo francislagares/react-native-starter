5256b87314aa46596a9b292f35f87f02aedf351a
Francis Lagares <francis.lagares@gmail.com>
2024-12-01 18:25:38 +0100

    test: implement custom renderer with SafeAreaProvider

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,36 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'jest-expo',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  displayName: 'MOBILE-EXPO',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
   verbose: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
   collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}', // Include TypeScript and TSX files
+    '!**/node_modules/**', // Exclude node_modules
+    '!**/ios/**', // Exclude iOS files
+    '!**/android/**', // Exclude Android files
+    '!**/tests/**', // Exclude test files
+    '!**/jest.config.ts', // Exclude Jest configuration file
+    '!**/*.d.ts', // Exclude all declaration files (.d.ts)
+  ],
+  clearMocks: true,
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/android/',
+    '/ios/',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Map `@/` to the `src` directory
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
 
 export default config;
